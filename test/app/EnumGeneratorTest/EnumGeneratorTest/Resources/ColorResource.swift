569ddcd2929f9e030c6624596b5287ce1e926492
k2moons <moonsocean@gmail.com>
2019-11-07 18:40:52 +0900

    Define Assets Color

--- test/app/EnumGeneratorTest/EnumGeneratorTest/Resources/ColorResource.swift
+++ test/app/EnumGeneratorTest/EnumGeneratorTest/Resources/ColorResource.swift
@@ -9,16 +9,24 @@
 import UIKit.UIColor
 
 extension AppResource.ColorResource {
-    struct Basic {
+    struct ManualDefined {
         static var backGroundColor: UIColor { return #colorLiteral(red: 0.1058823529, green: 0.1058823529, blue: 0.1058823529, alpha: 0.4) }
     }
 }
+
+extension AppResource.ColorResource {
+    struct Assets {
+        static var pPink: UIColor { return UIColor(named: "p_pink")! }
+    }
+}
+
 
 // MARK: - Test
 
 extension AppResourceTest {
     func testColorResource() {
-        let _ = AppResource.ColorResource.Basic.backGroundColor
-        let _ = R.Color.Basic.backGroundColor
+        let _ = AppResource.ColorResource.ManualDefined.backGroundColor
+        let _ = R.Color.ManualDefined.backGroundColor
+        let _ = R.Color.Assets.pPink
     }
 }
